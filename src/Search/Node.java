c97ec1e965506cacfe90124b2d7c07dc3ef9ce76
BT_1N3_11 <BT_1N3_11@BT_1N3_11.bktech.local>
2017-03-30 13:36:46 -0400

    Working on distanceForm

--- src/Search/Node.java
+++ src/Search/Node.java
@@ -14,13 +14,47 @@ public class Node implements Comparable{
     //    A Unique Identifier for our node
     public String cities;
     //    An arraylist containing a list of Nodes that
-//    This node is directly connected to - It's child nodes.
+    //    This node is directly connected to - It's child nodes.
     private ArrayList<Node> children = new ArrayList<>(); 
     private ArrayList<Integer> childrenCost = new ArrayList<>(); //acts as the row for the adjMatrix
     private int minDistance = Integer.MAX_VALUE;
+    private int posX, posY;
+    private double disFromGoal, disFromStart, cost;
     private Node parent;
 
-    public int getMinDistance() {
+    public int getPosX() {
+		return posX;
+	}
+
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+	public int getPosY() {
+		return posY;
+	}
+
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+
+	public double getDisFromGoal() {
+		return disFromGoal;
+	}
+
+	public void setDisFromGoal(double disFromGoal) {
+		this.disFromGoal = disFromGoal;
+	}
+
+	public double getDisFromStart() {
+		return disFromStart;
+	}
+
+	public void setDisFromStart(double disFromStart) {
+		this.disFromStart = disFromStart;
+	}
+
+	public int getMinDistance() {
 		return minDistance;
 	}
 
@@ -93,7 +127,22 @@ public class Node implements Comparable{
      * @return positive if this is greater, negative is this is less, 0 if they are equal
      */
 	public int compareTo(Object arg0) {
-		return this.getMinDistance() - ((Node) arg0).getMinDistance();
+		return (int) (this.cost - ((Node) arg0).getCost());
+	}
+
+	private double getCost() {
+		// TODO Auto-generated method stub
+		return cost;
+	}
+	
+	private void setCost(double cost) {
+		// TODO Auto-generated method stub
+		this.cost = cost;
+	}
+	
+	private double distanceFrom (Node n)
+	{
+		return Math.sqrt((posX - n.getPosX())*(posX - n.getPosX()) + (posY - n.getPosY())*(posY - n.getPosY()));
 	}
 
 	
